08ee3d73c9c10d479f43f49a4526016c10079283
bessax <andrebessax@gmail.com>
2024-10-07 08:50:08 -0300

    Adicionar arquivos de projeto.

---
+++ dotnet_async/Program.cs
@@ -0,0 +1,11 @@
+﻿void LerArqquivo()
+{
+    
+    string content = File.ReadAllText("arquivo.txt");
+    Thread.Sleep(5000);
+    Console.WriteLine("Arquivo lido.\n");
+    Console.WriteLine(content);
+}
+Console.WriteLine("Lendo arquivo em uma thread...");
+Console.ReadKey();
+
