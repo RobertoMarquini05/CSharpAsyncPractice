c0d7e87c3d5e069d33b12c80518c493ea23cb4eb
bessax <andrebessax@gmail.com>
2024-10-07 08:51:08 -0300

    #update: Inicial

--- dotnet_async/Program.cs
+++ dotnet_async/Program.cs
@@ -1,11 +1,1 @@
-﻿void LerArqquivo()
-{
-    
-    string content = File.ReadAllText("arquivo.txt");
-    Thread.Sleep(5000);
-    Console.WriteLine("Arquivo lido.\n");
-    Console.WriteLine(content);
-}
-Console.WriteLine("Lendo arquivo em uma thread...");
-Console.ReadKey();
-
+﻿
